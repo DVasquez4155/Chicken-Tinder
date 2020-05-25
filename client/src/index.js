d695508e5d3fcdd936931445d6d7e47a2ac51cdf
Daniel V <main@dvasquez4155.com>
2020-05-24 22:11:17 -0700

    Added bootstrap from node_modules

--- client/src/index.js
+++ client/src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 require('./index.css')
+require('bootstrap/dist/css/bootstrap.min.css')
 
 ReactDOM.render(<App />, document.getElementById("root"));
