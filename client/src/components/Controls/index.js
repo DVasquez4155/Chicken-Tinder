e6710814a6a0b71b62d016f54d8ecde34e19404c
Daniel V <main@dvasquez4155.com>
2020-05-24 22:12:49 -0700

    Added onclicks and updated look

--- client/src/components/Controls/index.js
+++ client/src/components/Controls/index.js
@@ -1,43 +1,31 @@
 import React from 'react';
 
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import { Container, Row, Button } from 'react-bootstrap';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faQuestion, faCheck, faTimes, faUndo, faShare } from '@fortawesome/free-solid-svg-icons'
 
 require('./index.css')
 
-const Controls = () => {
+const Controls = (props) => {
     return (
-        <Container className='controls'>
+        <Container className='controls' fluid>
             <Row className='interactive-icon'>
-                <Col>
-                    <Button className='times'>
-                        <FontAwesomeIcon icon={faTimes}/>
-                    </Button>
-                </Col>
-                <Col>
-                    <Button className='question'>
-                        <FontAwesomeIcon icon={faQuestion}/>
-                    </Button>
-                </Col>
-                <Col>
-                    <Button className='check'>
-                        <FontAwesomeIcon icon={faCheck}/>
-                    </Button>
-                </Col>
-            </Row>
-            <Row className='interactive-icon'>
-                <Col>
-                        <Button className='undo'>
-                            <FontAwesomeIcon icon={faUndo}/>
-                        </Button>
-                    </Col>
-                <Col>
-                    <Button className='share'>
-                        <FontAwesomeIcon icon={faShare}/>
-                    </Button>
-                </Col>
+                <Button onClick={()=>{props.decreaseCount()}} className='undo'>
+                    <FontAwesomeIcon icon={faUndo}/>
+                </Button>
+                <Button onClick={()=>{props.incrementCount()}} className='times large'>
+                    <FontAwesomeIcon size="2x" icon={faTimes}/>
+                </Button>
+                <Button onClick={()=>{props.incrementCount()}} className='question'>
+                    <FontAwesomeIcon icon={faQuestion}/>
+                </Button>
+                <Button onClick={()=>{props.incrementCount()}} className='check large'>
+                    <FontAwesomeIcon size="2x" icon={faCheck}/>
+                </Button>
+                <Button className='share'>
+                    <FontAwesomeIcon icon={faShare}/>
+                </Button>
             </Row>
         </Container>
     );
