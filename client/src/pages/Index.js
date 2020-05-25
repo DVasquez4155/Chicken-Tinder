bbe1680eaa6961668fa420b19b8b673e1442b0bc
Daniel V <main@dvasquez4155.com>
2020-05-24 22:10:28 -0700

    Updated temporary database and updated index page

--- client/src/pages/Index.js
+++ client/src/pages/Index.js
@@ -1,15 +1,39 @@
-import React, { Component } from 'react';
+import React from "react";
+import { Container } from "react-bootstrap";
 
 import BusinessCard from "../components/BusinessCard";
+import Controls from "../components/Controls";
 
-import db from "../Example.json"
-class Index extends Component {
-  state = {
-    db
+import API from "../utils/API";
+class Index extends React.Component {
+  constructor(props) {
+    super(props);
+    // this.state = {date: new Date()};
+      this.state = {
+        businesses: API.getBusinesses(),
+        count: 0
+      }
   }
-  render() { 
+  render() {
+    var functions = {
+      incrementCount: () => {
+        this.setState({
+          count: this.state.count + 1
+        });
+      },
+      decreaseCount: () => {
+        this.setState({
+          count: this.state.count - 1
+        });
+      }
+    }
     return (
-      <BusinessCard {...db.businesses[0]} />
+      <main>
+        <Container className='view'>
+          <BusinessCard {...this.state.businesses[this.state.count]} />
+          <Controls {...functions} />
+        </Container>
+      </main>
     );
   }
 }
