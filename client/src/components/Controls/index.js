e70dadfa7cb09e6095fe3dab778c5e85a53fb025
Daniel V <main@dvasquez4155.com>
2020-05-24 22:39:12 -0700

    Changed coloring

--- client/src/components/Controls/index.js
+++ client/src/components/Controls/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { Container, Row, Button } from 'react-bootstrap';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faQuestion, faCheck, faTimes, faUndo, faShare } from '@fortawesome/free-solid-svg-icons'
+import { faQuestion, faCheck, faTimes, faUndo, faBookmark } from '@fortawesome/free-solid-svg-icons'
 
 require('./index.css')
 
@@ -11,20 +11,21 @@ const Controls = (props) => {
     return (
         <Container className='controls' fluid>
             <Row className='interactive-icon'>
-                <Button onClick={()=>{props.decreaseCount()}} className='undo'>
+                <Button onClick={()=>{props.decreaseCount()}} variant="outline-warning">
                     <FontAwesomeIcon icon={faUndo}/>
                 </Button>
-                <Button onClick={()=>{props.incrementCount()}} className='times large'>
+                <Button onClick={()=>{props.incrementCount()}} className='times large' variant="outline-danger">
                     <FontAwesomeIcon size="2x" icon={faTimes}/>
                 </Button>
-                <Button onClick={()=>{props.incrementCount()}} className='question'>
+                <Button onClick={()=>{props.incrementCount()}} variant="outline-secondary">
+                {/* <Button onClick={()=>{props.incrementCount()}} className='question'> */}
                     <FontAwesomeIcon icon={faQuestion}/>
                 </Button>
-                <Button onClick={()=>{props.incrementCount()}} className='check large'>
+                <Button onClick={()=>{props.incrementCount()}} className='check large' variant="outline-success">
                     <FontAwesomeIcon size="2x" icon={faCheck}/>
                 </Button>
-                <Button className='share'>
-                    <FontAwesomeIcon icon={faShare}/>
+                <Button onClick={()=>{props.incrementCount()}} variant="outline-primary">
+                    <FontAwesomeIcon icon={faBookmark}/>
                 </Button>
             </Row>
         </Container>
