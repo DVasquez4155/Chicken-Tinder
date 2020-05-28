3047c4365ff3068e2d34e528e9809f87ec2faa38
Daniel V <main@dvasquez4155.com>
2020-05-27 20:19:29 -0700

    Added base pages

---
+++ client/src/pages/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container, Col, Row } from "react-bootstrap";
+
+function Login() {
+    return (
+        <main>
+    
+        </main>
+    );
+}
+
+export default Login;
