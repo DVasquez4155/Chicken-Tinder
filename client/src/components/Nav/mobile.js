9e266feda693ad5a0ffbe60d63591d9002a78d45
Daniel V <main@dvasquez4155.com>
2020-06-01 20:08:04 -0700

    Added icons for mobile view

--- client/src/components/Nav/mobile.js
+++ client/src/components/Nav/mobile.js
@@ -1,23 +1,26 @@
 import React from "react";
 import RestaurantGroup from "../RestaurantGroup";
-import { Row, Container, Col } from "react-bootstrap";
+import { Row, Container, Col, Button } from "react-bootstrap";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar, faUser, faBookmark } from '@fortawesome/free-solid-svg-icons'
 
 require('./index.css')
 
 function Nav() {
     return (
         <main className="d-md-none navigation">
-                <Container >
-                    <Row>
-                        <Col>
-                            Profile
-                        </Col>
-                        <Col>
-                            Favorites?
-                        </Col>
-                        <Col>
-                            Matches
-                        </Col>
+                <Container>
+                    <Row className='interactive-icon nav-icon'>
+                        <Button className="default" variant="link">
+                            <FontAwesomeIcon className='default' icon={faUser}/>
+                        </Button>
+                        <Button  className="default" variant="link">
+                            <FontAwesomeIcon className='default' icon={faBookmark}/>
+                        </Button>
+                        <Button  className="default" variant="link">
+                            <FontAwesomeIcon className='default' icon={faStar}/>
+                        </Button>
                     </Row>
                 </Container>
         </main>
