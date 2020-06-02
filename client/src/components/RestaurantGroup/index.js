c61042661ac02d0e96493da65da3e1b292e8d246
Conrad Rodgers <conrad13500@gmail.com>
2020-06-01 20:42:24 -0700

    side Match list

--- client/src/components/RestaurantGroup/index.js
+++ client/src/components/RestaurantGroup/index.js
@@ -1,19 +1,44 @@
-import React from 'react';
-import { Container, Col, Row, ListGroup } from "react-bootstrap";
+import React from "react";
+import API from "../../utils/API";
+import { Container, Col, Row, ListGroup, Image } from "react-bootstrap";
+import "./index.css";
+var DB = API.getBusinesses();
+console.log(DB);
 
 function RestaurantGroup(props) {
-    return (
-        <main>
-            <ListGroup>
-                <ListGroup.Item>
-                    Business 1
-                </ListGroup.Item>
-                <ListGroup.Item>
-                    Business 2
-                </ListGroup.Item>
-            </ListGroup>
-        </main>
-    );
+  return (
+    <main>
+      <ListGroup className>
+        <ListGroup.Item>
+          <h5>
+            {" "}
+            {DB[0].name}
+            {"--"}
+            {DB[0].price}
+          </h5>
+
+          <span class="side">
+            <Image src={DB[0].image_url} roundedCircle />
+            {DB[0].display_phone}
+          </span>
+        </ListGroup.Item>
+        <ListGroup.Item>
+          {" "}
+          <h5>
+            {" "}
+            {DB[0].name}
+            {"--"}
+            {DB[0].price}
+          </h5>
+          <span class="side">
+            <Image src={DB[0].image_url} roundedCircle />
+
+            {DB[0].display_phone}
+          </span>
+        </ListGroup.Item>
+      </ListGroup>
+    </main>
+  );
 }
 
 export default RestaurantGroup;
