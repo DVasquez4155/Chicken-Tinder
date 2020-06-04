95ed4dcedebaf9a23d4bc655f9ae3a22a481c047
Conrad Rodgers <conrad13500@gmail.com>
2020-06-03 20:37:28 -0700

    psuh

--- client/src/components/RestaurantGroup/index.js
+++ client/src/components/RestaurantGroup/index.js
@@ -7,37 +7,26 @@ console.log(DB);
 
 function RestaurantGroup(props) {
   return (
-    <main>
-      <ListGroup variant="flush">
-        <ListGroup.Item border="success" className="side">
-          <h5>
-            {" "}
-            {DB[0].name}
-            {"--"}
-            {DB[0].price}
-          </h5>
+    <main className="scroll">
+      <ListGroup variant="flush" variant="danger">
+        {DB.map((business) => (
+          <ListGroup.Item border="success" className="side">
+            <h5>
+              {" "}
+              {business.name}
+              {"  "}
+              {business.price}
+            </h5>
 
-          <span>
-            <Image src={DB[0].image_url} roundedCircle />
+            <span>
+              <Image src={business.image_url} roundedCircle />
 
-            <a href={`tel:${DB[0].display_phone}`}>{DB[0].display_phone}</a>
-          </span>
-        </ListGroup.Item>
-
-        <ListGroup.Item className="side">
-          {" "}
-          <h5>
-            {" "}
-            {DB[0].name}
-            {"--"}
-            {DB[0].price}
-          </h5>
-          <span>
-            <Image src={DB[0].image_url} roundedCircle />
-
-            <a href={`tel:${DB[0].display_phone}`}>{DB[0].display_phone}</a>
-          </span>
-        </ListGroup.Item>
+              <a href={`tel:${business.display_phone}`}>
+                {business.display_phone}
+              </a>
+            </span>
+          </ListGroup.Item>
+        ))}
       </ListGroup>
     </main>
   );
