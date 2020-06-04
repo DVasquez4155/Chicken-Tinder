11a222c3019213de79c2b8f2b17be10dec160439
David Pendley <dpendley401@gmail.com>
2020-06-03 19:25:54 -0700

    Created basic setup for login and register pages with react bootstrap

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,12 +1,31 @@
 import React from "react";
-import { Container, Col, Row } from "react-bootstrap";
+import { Container, Col, Row, Form, Button } from "react-bootstrap";
 
 function Login() {
-    return (
-        <main>
-    
-        </main>
-    );
+  return (
+    <main>
+      <Form>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </main>
+  );
 }
 
 export default Login;
