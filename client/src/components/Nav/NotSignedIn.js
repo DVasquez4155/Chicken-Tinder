455257353b2cef6c9ea3f9028d287b31f6d54360
David Pendley <dpendley401@gmail.com>
2020-06-10 19:39:14 -0700

    Updated navbar buttons

--- client/src/components/Nav/NotSignedIn.js
+++ client/src/components/Nav/NotSignedIn.js
@@ -2,29 +2,51 @@ import React from "react";
 import RestaurantGroup from "../RestaurantGroup";
 import { Button } from "react-bootstrap";
 
-require('./index.css')
+require("./index.css");
 
 function Nav(props) {
-    return (
-        <main>
-            <nav className="navbar navbar-dark navbar-expand-lg bg-dark ">
-                <div className="container">
-                    <a className="navbar-brand logo" href="/">What's For Dinner</a>
-                    <button data-toggle="collapse" className="navbar-toggler" data-target="#navbarNav">
-                        <span className="sr-only">Toggle navigation</span>
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul id="links" className="nav navbar-nav ml-auto">
-                            <Button variant="success">Create Session</Button>
-                            <li className="nav-item" role="presentation"><a className="nav-link" href="/login">Login</a></li>
-                            <li className="nav-item" role="presentation"><a className="nav-link" href="/register">Register</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </main>
-    );
+  return (
+    <main>
+      <nav className="navbar navbar-dark navbar-expand-lg bg-dark ">
+        <div className="container">
+          <a className="navbar-brand logo" href="/">
+            What's For Dinner
+          </a>
+          <button
+            data-toggle="collapse"
+            className="navbar-toggler"
+            data-target="#navbarNav"
+          >
+            <span className="sr-only">Toggle navigation</span>
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul id="links" className="nav navbar-nav ml-auto">
+              {/* <Button variant="success">Create Session</Button> */}
+              <li className="nav-item" role="presentation">
+                <Button
+                  variant="outline-warning"
+                  className="nav-link"
+                  href="/login"
+                >
+                  Login
+                </Button>
+              </li>
+              <li className="nav-item" role="presentation">
+                <Button
+                  variant="outline-warning"
+                  className="nav-link"
+                  href="/register"
+                >
+                  Register
+                </Button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </main>
+  );
 }
 
 export default Nav;
